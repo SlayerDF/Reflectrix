cf5f837c26faa3f100c9283ca3acf13d8eeb52c7
Nikita Tulin <ntulin.work@gmail.com>
2025-05-04 14:40:04 -0300

    Fix UniTrait AddTrait bug & add debug methods calls in inspector

--- Assets/Libraries/UniTrait/Runtime/UniTraitContainer.cs
+++ Assets/Libraries/UniTrait/Runtime/UniTraitContainer.cs
@@ -145,13 +145,15 @@ namespace UniTrait
 
         public void AddTrait<T>(T trait) where T : IUniTrait
         {
-            if (traitsIndexes.ContainsKey(typeof(T)))
+            var type = trait.GetType();
+
+            if (traitsIndexes.ContainsKey(type))
             {
-                throw new ArgumentException($"Trait of type {typeof(T)} already exists");
+                throw new ArgumentException($"Trait of type {type} already exists");
             }
 
             traitsList.Add(trait);
-            traitsIndexes[typeof(T)] = traitsList.Count - 1;
+            traitsIndexes[type] = traitsList.Count - 1;
 
             trait.InjectContainer(this);
             trait.InjectEventBus(eventBus);
@@ -168,6 +170,11 @@ namespace UniTrait
         public T GetTrait<T>() where T : IUniTrait
         {
             return traitsIndexes.TryGetValue(typeof(T), out var traitIndex) ? (T)traitsList[traitIndex] : default;
+        }
+
+        public IEnumerable<IUniTrait> GetTraits()
+        {
+            return traitsList;
         }
 
         private void AutoAddTraits()
---
+++ Assets/Libraries/UniTrait/Runtime/UniTraitDebugActionAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UniTrait
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class UniTraitDebugActionAttribute : Attribute
+    {
+        public UniTraitDebugActionAttribute(string label)
+        {
+            Label = label;
+        }
+
+        public string Label { get; }
+    }
+}
