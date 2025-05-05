0004b6a076c60e02502725d44960912f5be995c3
Nikita Tulin <ntulin.work@gmail.com>
2025-05-05 08:49:37 -0300

    Implement Splitter

---
+++ Assets/Entities/Devices/Splitter/Scripts/Splitter.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using Reflectrix.LaserBeam;
+using Reflectrix.Traits;
+using UniTrait;
+using UnityEngine;
+
+namespace Reflectrix.Entities.Devices
+{
+    public class Splitter : UniTraitContainer, IBeamReceiverAndEmitter
+    {
+        #region Serialized Fields
+
+        [SerializeField]
+        private LaserReceiverTrait laserReceiverTrait;
+
+        [SerializeField]
+        private LaserEmitterTrait laserEmitterTrait;
+
+        #endregion
+
+        #region IBeamReceiverAndEmitter Members
+
+        public ILaserBeamPoint[] ReceiveAndEmit(ILaserBeamPoint[] beamPoints)
+        {
+            var receivedBeamPoints = laserReceiverTrait.Receive(beamPoints);
+
+            if (receivedBeamPoints.Length == 0)
+            {
+                return new ILaserBeamPoint[] { };
+            }
+
+            var incomingIntensity = receivedBeamPoints.Sum(x => x.Intensity);
+            var emittedBeamPoints = laserEmitterTrait.Emit();
+            var splitIntensity = incomingIntensity / emittedBeamPoints.Length;
+
+            return emittedBeamPoints
+                .Select<ILaserBeamPoint, ILaserBeamPoint>(x =>
+                    new LaserBeamPoint(x.Origin, x.Direction, splitIntensity, x.Color)).ToArray();
+        }
+
+        #endregion
+    }
+}
