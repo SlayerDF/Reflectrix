7c2980cdf7675830211e7a9b5d9fac4d46530e78
Nikita Tulin <ntulin.work@gmail.com>
2025-05-05 10:13:05 -0300

    Add main menu

---
+++ Assets/UI/Scripts/SceneLoaderButton.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Reflectrix
+{
+    public class SceneLoaderButton : MonoBehaviour
+    {
+        #region Serialized Fields
+
+        [SerializeField]
+        private Button button;
+
+        [SerializeField]
+        private string sceneName;
+
+        #endregion
+
+        #region Event Functions
+
+        private void OnEnable()
+        {
+            button.onClick.AddListener(LoadScene);
+        }
+
+        private void OnDisable()
+        {
+            button.onClick.RemoveListener(LoadScene);
+        }
+
+        #endregion
+
+        private void LoadScene()
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
