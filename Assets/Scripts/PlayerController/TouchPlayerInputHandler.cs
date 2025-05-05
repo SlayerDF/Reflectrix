640adc08a4d2c91556ea3ec52cf21013c6b84c9f
Elina Savina <savina.elya@gmail.com>
2025-05-05 13:38:55 -0300

    Fix touch

--- Assets/Scripts/PlayerController/TouchPlayerInputHandler.cs
+++ Assets/Scripts/PlayerController/TouchPlayerInputHandler.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
@@ -6,10 +7,14 @@ namespace Reflectrix.PlayerController
 {
     internal class TouchPlayerInputHandler : MonoBehaviour, IPlayerInputHandler
     {
+        private const float tapMaxDuration = 0.2f;
+        private const float TapMaxMovement = 30; // pixels
         private PlayerController playerController;
         private PlayerControls controls;
         private bool isInitialized = false;
         private bool processTouch = false;
+        private Vector2 touchStartPosition;
+        private bool isTouching;
 
         public void Initialize(PlayerController playerController, PlayerControls controls)
         {
@@ -21,15 +26,38 @@ namespace Reflectrix.PlayerController
             this.playerController = playerController;
             this.controls = controls;
 
-            controls.Player.Touch.performed += OnTouchPerformed;
+            controls.Player.Touch.started += OnTouchStarted;
+            controls.Player.Touch.canceled += OnTouchCanceled;
             isInitialized = true;
+        }
+
+        private void OnTouchCanceled(InputAction.CallbackContext context)
+        {
+            if (!isTouching)
+                return;
+
+            isTouching = false;
+            var endPos = Touchscreen.current.position.ReadValue();
+            var movement = Vector2.Distance(endPos, touchStartPosition);
+
+            if (movement <= TapMaxMovement)
+            {
+                processTouch = true;
+            }
+        }
+
+        private void OnTouchStarted(InputAction.CallbackContext context)
+        {
+            isTouching = true;
+            touchStartPosition = Touchscreen.current.position.ReadValue();
         }
 
         private void OnDestroy()
         {
             if (controls != null)
             {
-                controls.Player.MouseLeftClick.performed -= OnTouchPerformed;
+                controls.Player.Touch.started -= OnTouchStarted;
+                controls.Player.Touch.canceled -= OnTouchCanceled;
             }
 
             isInitialized = false;
@@ -46,17 +74,11 @@ namespace Reflectrix.PlayerController
             {
                 if (!EventSystem.current.IsPointerOverGameObject())
                 {
-                    var pointerPosition = Touchscreen.current.position.ReadValue();
-                    playerController.OnClick(pointerPosition);
+                    playerController.OnClick(touchStartPosition);
                 }
 
                 processTouch = false;
             }
-        }
-
-        private void OnTouchPerformed(InputAction.CallbackContext context)
-        {
-            processTouch = true;
         }
     }
 }
